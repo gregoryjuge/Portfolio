ac92d60f1ca434d76482f0df28fd0a02578328bd
GregJuge <gregoryjuge5@gmail.com>
2021-02-03 19:45:17 +0100

    init

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,30 @@
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+import Home from "./components/Home";
+import Projects from "./components/Projects;"
+import About from "./components/About";
+import Contact from "./components/Contact";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/Projects" component={Projects} />
+          <Route path="/About" component={About} />
+          <Route path="/Contact" component={Contact} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ portfolio/src/components/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../css/home.css";
+import Projects from "./Projects";
+import About from "./About";
+import Contact from "./Contact";
+
+function Home() {
+  return (
+    <div className="home-page">
+      <div className="navbar">
+        <h1>Grégory JUGE</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
