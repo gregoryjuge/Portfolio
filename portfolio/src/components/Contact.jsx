553415feea44375a16fa610558886988556e52de
GregJuge <gregoryjuge5@gmail.com>
2021-02-05 20:09:52 +0100

    css return button

--- portfolio/src/components/Contact.jsx
+++ portfolio/src/components/Contact.jsx
@@ -4,8 +4,19 @@ import "../css/contact.css";
 function Contact() {
   return (
     <section id="contact">
-      
       <h1 class="section-header">CONTACT</h1>
+
+      <div class="button_cont" align="center">
+        <a
+          class="button-home"
+          href="/"
+          // target="_blank"
+          // rel="nofollow noopener"
+        >
+          HOME
+        </a>
+      </div>
+
       <div className="form-container">
         <input
           type="text"
@@ -29,22 +40,21 @@ function Contact() {
           class="form-control"
           className="form-control-text"
           rows="10"
-          placeholder="Description de votre projet" 
+          placeholder="Description de votre projet"
           name="message"
           required
         ></textarea>
 
-        <button
-          class="btn btn-primary send-button"
-          id="submit"
-          type="submit"
-          value="SEND"
-        >
-          <div class="button">
-            <i class="fa fa-paper-plane"></i>
-            <span class="send-text">ENVOYER</span>
-          </div>
-        </button>
+        <div class="button_cont" align="center">
+          <a
+            class="button icon-send"
+            href=""
+            target="_blank"
+            rel="nofollow noopener"
+          >
+            ENVOYER
+          </a>
+        </div>
       </div>
     </section>
   );
