ab226b836443cdf83b99dd165a33ccf95a29e326
Gregory Juge <gregoryjuge@MacBook-Pro-de-Gregory.local>
2021-05-03 19:44:52 +0200

    onclick 2

--- portfolio/src/components/Menu.jsx
+++ portfolio/src/components/Menu.jsx
@@ -1,40 +1,27 @@
 import React, { Component } from "react";
 
-export default class Menu extends Component {
+export default class Menu extends React.Component {
   constructor(props) {
     super(props);
-    this.handleMenuOnClick = this.handleMenuOnClick.bind(this);
-    this.handleMenuOffClick = this.handleMenuOffClick.bind(this);
-    this.state = { isMenuOn: false };
+    this.state = {isMenuOn: false};
+
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  handleMenuOnClick() {
-    this.setState({ isMenuOn: true });
-  }
-
-  handleMenuOffClick() {
-    this.setState({ isMenuOn: false });
+  handleClick() {
+    this.setState(state => ({
+      isMenuOn: !state.isMenuOn
+    }));
   }
 
   render() {
-    const isMenuOn = this.state.isMenuOn;
-    console.log("this.state.isMenuOn");
-    let button;
-
-    if (isMenuOn) {
-      button = <MenuOn onClick={this.handleisMenuOffClick} />;
-    } else {
-      button = <MenuOff onClick={this.handleisMenuOnClick} />;
-    }
-
-    return <div>{button}</div>;
+    return (
+      <button onClick={this.handleClick}>{this.state.isMenuOn ? 'ON' : 'OFF'}</button>
+    )
   }
 }
 
-function MenuOff(props) {
-  return <button onClick={props.onClick}>lignes</button>;
-}
-
-function MenuOn(props) {
-  return <button onClick={props.onClick}>croix</button>;
-}
+// ReactDom.render(
+//   <Menu />,
+//   document.getElementById('root')
+// );
